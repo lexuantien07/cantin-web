6faa1e175f241846a4b4f7b886103432cc745734
20120593 <20120593@student.hcmus.edu.vn>
2023-05-24 21:23:13 +0700

    fix env error

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,12 +1,21 @@
 import mysql from 'mysql2/promise';
-
+import env from 'dotenv';
+env.config();
 const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    database: process.env.DBDATABASE,
+    password: process.env.DBPASSWORD,
     port: process.env.DBPORT
 });
+
+// const pool = mysql.createPool({
+//     host: 'containers-us-west-27.railway.app',
+//     user: 'root',
+//     database: 'railway',
+//     password: '6zH7IT2CUzxWEkUWAnL0',
+//     port: '7203'
+// });
 
 // const pool = mysql.createPool({
 //     host: 'localhost',
